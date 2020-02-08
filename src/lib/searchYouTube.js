f51342754926f5aeb7284405631132ba3d178656
Matt Goff <mgoff72@gmail.com>
2020-02-08 14:25:41 -0700

    Advanced Content - Refactor searcYouTube to use fetch

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,22 +1,45 @@
+// var searchYouTube = ({key, query, max = 5}, callback) => {
+//   $.get('https://www.googleapis.com/youtube/v3/search', {
+//     part: 'snippet',
+//     key: key,
+//     q: query,
+//     maxResults: max,
+//     type: 'video',
+//     videoEmbeddable: 'true'
+//   })
+//     .done(({items}) => {
+//       if (callback) {
+//         callback(items);
+//       }
+//     })
+//     .fail(({responseJSON}) => {
+//       responseJSON.error.errors.forEach((err) =>
+//         console.error(err)
+//       );
+//     });
+// };
+
+
+// https://www.googleapis.com/youtube/v3/search/part=snippet&key=AIzaSyAi3PObXqu7aQzc4idXaGeQWdwmRKQU-NQ&q=boxer+puppies&maxResults=5&type=video&videoEmbeddable=true
+
 var searchYouTube = ({key, query, max = 5}, callback) => {
-  $.get('https://www.googleapis.com/youtube/v3/search', {
+  fetch('https://www.googleapis.com/youtube/v3/search/?' + new URLSearchParams({
     part: 'snippet',
     key: key,
     q: query,
     maxResults: max,
     type: 'video',
     videoEmbeddable: 'true'
-  })
-    .done(({items}) => {
+  }))
+    .then( response => response.json())
+    .then( data => {
+      console.log(data);
       if (callback) {
-        callback(items);
+        callback(data.items);
       }
     })
-    .fail(({responseJSON}) => {
-      responseJSON.error.errors.forEach((err) =>
-        console.error(err)
-      );
-    });
+    .catch((error) => console.error(error));
 };
+
 
 export default searchYouTube;
